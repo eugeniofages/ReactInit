77500fd6f1baabdba5e9f89e42cb2487e18e1b51
Eugenio <eugeniobalbastrofages@gmail.com>
2022-05-16 09:25:16 -0300

    subida de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Layout from './layouts/Layout'
+import Inicio from './pages/Inicio'
+import NuevoCliente from './pages/NuevoCliente'
+import EditarCliente from './pages/EditarCliente'
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+  
+      <Route path="/clientes" element={<Layout />} >
+          <Route  index element ={<Inicio />} />
+          <Route  path="nuevo" element ={<NuevoCliente />} />
+          <Route  path="editar/:id" element ={<EditarCliente />} />
+
+      </Route >
+
+
+
+    
+
+
+
+
+      </Routes>
+    
+    
+    
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Formulario = () => {
+    return (
+        <div className='bg-white mt-10'>
+            <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>Agregar Cliente</h1>
+            <form action="">
+
+
+
+
+
+
+
+
+            </form>
+
+
+        </div>
+    )
+}
+
+export default Formulario
